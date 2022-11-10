2dd64338439d3b8cffc6b83ecdcf88616c0b08b9
koyoujin6759 <rhdbwls853@naver.com>
2022-11-10 09:49:03 +0900

    vue footer 추가

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,3 +1,4 @@
+export { default as Footer } from '../../components/Footer.vue'
 export { default as Header } from '../../components/Header.vue'
 
 // nuxt/nuxt.js#8607
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,18 @@
     <div id="app">
         <Header />        
         <Nuxt />   
-        dd     
+        <Footer />
     </div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 export default {
     name: 'default',
     components: {
-        Header
+        Header,
+        Footer
     }
 };
 </script>
